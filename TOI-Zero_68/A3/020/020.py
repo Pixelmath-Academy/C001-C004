382ec855df4bc9545d576f8c559bd84995e9bcb9
tawdces <148196815+tawdces@users.noreply.github.com>
2025-05-10 18:08:12 +0700

    update sol

--- TOI-Zero_68/A3/020/020.py
+++ TOI-Zero_68/A3/020/020.py
@@ -1,1 +1,18 @@
- 
+h1, h2, b1, b2 = map(int, input().split())
+x, y = map(int, input().split())
+
+max_soldiers = 0
+
+for i in range(x + 1):
+    for j in range(x - i + 1):
+        for a in range(y + 1):
+            for b in range(y - a + 1):
+                hh1 = i + b
+                hh2 = j + a
+                bb1 = i + a
+                bb2 = j + b
+                
+                if hh1 <= h1 and hh2 <= h2 and bb1 <= b1 and bb2 <= b2:
+                    max_soldiers = max(max_soldiers, i + j + a + b)
+
+print(max_soldiers) 
