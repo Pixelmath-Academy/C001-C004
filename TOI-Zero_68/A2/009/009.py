096939b01e5f6f80480ba113b83876134774d965
tawdces <148196815+tawdces@users.noreply.github.com>
2025-05-10 18:03:29 +0700

    update sol

--- TOI-Zero_68/A2/009/009.py
+++ TOI-Zero_68/A2/009/009.py
@@ -1,28 +1,44 @@
-n, pass_player = map(int, input().split())
-tour = [[0] * (n + 1) for _ in range(n + 1)]
+import sys
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        tour[i][j] = int(input())
+def main():
+    n, pass_player = map(int, sys.stdin.readline().split())
 
-checklist = list(range(1, n + 1))
+    tour = [[0] * (n + 1) for _ in range(n + 1)]
 
-while len(checklist) > 1:
-    winner = []
-    for i in range(0, len(checklist), 2):
-        x, y = checklist[i], checklist[i + 1]
-        if tour[x][y] == x:
-            if pass_player == y:
-                pass_player = 0
-                winner.append(y)
+    for i in range(1, n + 1):
+        row = list(map(int, sys.stdin.readline().split()))
+        for j in range(1, n + 1):
+            tour[i][j] = row[j - 1]
+
+    checklist = list(range(1, n + 1))
+
+    while len(checklist) > 1:
+        winner = []
+
+        i = 0
+        while i < len(checklist):
+            if i + 1 >= len(checklist):
+                winner.append(checklist[i])
+                i += 1
             else:
-                winner.append(x)
-        else:
-            if pass_player == x:
-                pass_player = 0
-                winner.append(x)
-            else:
-                winner.append(y)
-    checklist = winner
+                x, y = checklist[i], checklist[i + 1]
+                if tour[x][y] == x:
+                    if pass_player == y:
+                        pass_player = 0
+                        winner.append(y)
+                    else:
+                        winner.append(x)
+                else:
+                    if pass_player == x:
+                        pass_player = 0
+                        winner.append(x)
+                    else:
+                        winner.append(y)
+                i += 2
 
-print(checklist[0]) 
+        checklist = winner
+
+    print(checklist[0])
+
+if __name__ == "__main__":
+    main()
