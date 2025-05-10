d1b96bbf80ee81359288a17bdb44f1176576503c
tawdces <148196815+tawdces@users.noreply.github.com>
2025-05-10 18:08:31 +0700

    update sol

--- TOI-Zero_68/A3/021/021.py
+++ TOI-Zero_68/A3/021/021.py
@@ -1,1 +1,14 @@
- 
+import math
+
+n, k = map(int, input().split())
+a = [int(input()) for _ in range(n)]
+a.sort()
+
+x = k * a[0]
+count = 1  # Start with 1 since we're checking from index 1
+
+for i in range(1, n):
+    if math.ceil(x / a[i]) == k:
+        count += 1
+
+print(count) 
