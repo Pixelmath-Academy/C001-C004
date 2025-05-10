bef1436e9ba18700b2d4b6d2aca72b7280952a2b
tawdces <148196815+tawdces@users.noreply.github.com>
2025-05-10 18:07:53 +0700

    update sol

--- TOI-Zero_68/A3/019/019.py
+++ TOI-Zero_68/A3/019/019.py
@@ -1,1 +1,17 @@
- 
+n, k = map(int, input().split())
+
+a = [0] * (n + 1)
+p = [0] * (n + 1)
+
+a[1:] = list(map(int, input().split()))
+
+for i in range(1, n + 1):
+    p[i] = max(a[i - 1], p[i - 1])
+
+x = list(map(int, input().split()))
+
+for idx in x:
+    if p[idx] >= a[idx]:
+        print(p[idx] - a[idx] + 1)
+    else:
+        print(0) 
