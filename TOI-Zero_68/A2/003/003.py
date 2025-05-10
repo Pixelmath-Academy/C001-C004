728439cec7eceb6afcb4a35e22844dc5cf8e020a
tawdces <148196815+tawdces@users.noreply.github.com>
2025-05-10 18:01:53 +0700

    update sol

--- TOI-Zero_68/A2/003/003.py
+++ TOI-Zero_68/A2/003/003.py
@@ -1,12 +1,13 @@
 n = int(input())
-tree = [0] * (n + 2)
 
-for i in range(1, n + 1):
-    tree[i] = int(input())
+tree = list(map(int, input().split()))
+
+tree = [0] + tree + [0]
 
 count = 0
+
 for i in range(1, n + 1):
-    if tree[i] > tree[i-1] and tree[i] > tree[i+1]:
+    if tree[i] > tree[i - 1] and tree[i] > tree[i + 1]:
         count += 1
 
-print(count) 
+print(count)
